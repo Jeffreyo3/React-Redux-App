72856199307041d32547490eb6583bdfe08591cd
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-02-24 13:16:08 -0800

    Create drop-down menu to select starting generation on initial API call
    Update styling syntax & increase line-height to left-bar text

--- src/components/PokemonListContainer.js
+++ src/components/PokemonListContainer.js
@@ -5,10 +5,15 @@ import './pokemon-list-container.css'
 import PokemonList from './PokemonList'
 
 const PokemonListContainer = props => {
-    const [number, setNumber] = useState(50);
+    const [number, setNumber] = useState(10);
+    const [generation, setGeneration] = useState(0)
     const handleChange = e => {
         e.preventDefault();
         setNumber(e.target.value);
+    }
+    const handleGenChange = e => {
+        e.preventDefault();
+        setGeneration(e.target.value);
     }
 
     return (
@@ -25,12 +30,25 @@ const PokemonListContainer = props => {
                     {props.data.results.length === 0 ? (
                         <>
                             <form>
-                                <label htmlFor="num-display">Number of Pokemon</label>
+                                <fieldset htmlFor="num-display">Num of Pokemon to display at a time: </fieldset>
                                 <input type="number" id="num-display" name="number" min="0" max="964" value={number} onChange={handleChange} />
+                            </form>
+                            <form>
+                                <fieldset htmlFor="gen-display">Starting Generation: </fieldset>
+                                {/* <legend>Generation</legend> */}
+                                <select id="gen-display" name="gen-display" onChange={handleGenChange}>
+                                    <option value={0}>Kanto</option>
+                                    <option value={151}>Johto</option>
+                                    <option value={251}>Hoenn</option>
+                                    <option value={386}>Sinnoh</option>
+                                    <option value={493}>Unova</option>
+                                    <option value={649}>Kalos</option>
+                                    <option value={721}>Alola</option>
+                                </select>
                             </form>
                             <button className="getButton"
                                 onClick={() => {
-                                    props.getNewPKMNData(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=${number}`);
+                                    props.getNewPKMNData(`https://pokeapi.co/api/v2/pokemon?offset=${generation}&limit=${number}`);
                                 }}> S T A R T </button>
                         </>) :
                         <button className="getButton"
@@ -50,13 +68,13 @@ const PokemonListContainer = props => {
                 {props.isLoading ? (
                     <div>Catching 'em All!</div>
                 ) : (
-                    <>
-                        <div className="pokemon-list">
-                            {props.results.map(pokemon => {
-                                return <PokemonList key={pokemon.url} pokemon={pokemon} addPokemon={props.addPokemon} />
-                            })}
-                        </div>
-                        <div style={{marginTop: "2%"}}/>
+                        <>
+                            <div className="pokemon-list">
+                                {props.results.map(pokemon => {
+                                    return <PokemonList key={pokemon.url} pokemon={pokemon} addPokemon={props.addPokemon} />
+                                })}
+                            </div>
+                            <div style={{ marginTop: "2%" }} />
                         </>
                     )}
             </div>
