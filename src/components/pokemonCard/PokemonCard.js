5427bf53b7b8b31f81eb81a9fd30f58898e13370
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-02-24 15:10:55 -0800

    Add background color depending on type
    Use ternary operator to check if props exist to keep react from attempting to map through props before render

--- src/components/pokemonCard/PokemonCard.js
+++ src/components/pokemonCard/PokemonCard.js
@@ -1,29 +1,60 @@
-import React from 'react';
-import CardHeader from './CardHeader'
-import './pokemon-card.css'
+import React from "react";
+import CardHeader from "./CardHeader";
+import "./pokemon-card.css";
 
-const PokemonCard = (props) => {
+const PokemonCard = props => {
 
-    return (
-        <div className="pokemonCard-border">
-            <div className="pokemonCard">
-                <CardHeader pokemon={props.pokemon} />
+    
+  return (
+    <div className="pokemonCard-border">
+        {/* Since props might be async, Need to run a check to see if they exist first */}
+        {props.pokemon.extra.id === null ? (
+        <div className={`pokemonCard`}>
+            <CardHeader pokemon={props.pokemon} />
 
-                {props.pokemon.extra.id === null ? (
-                    <p>Loading...</p>
-                ) : (
-                        <>
-                            <img className="sprite" alt={`A pokemon named ${props.pokemon.name}`} src={props.pokemon.extra.sprites.front_default} />
-                            <div className="add-remove-button" onClick={props.handleAddPokemon}>
-                                CATCH
-                        </div>
-                        </>
-                    )
-                }
-            </div >
+            {props.pokemon.extra.id === null ? (
+            <p>Loading...</p>
+            ) : (
+            <>
+                <img
+                className="sprite"
+                alt={`A pokemon named ${props.pokemon.name}`}
+                src={props.pokemon.extra.sprites.front_default}
+                />
+                <div className="add-remove-button" onClick={props.handleAddPokemon}>
+                CATCH
+                </div>
+            </>
+            )}
         </div>
-    )
-}
+        ) : (
+            // If extra props exist, grab the slot 1 type and add it to pokemonCard class name.
+            // This API keeps slot 1 typing at the end of the array so my index will be the array length -1
+            <div className={`pokemonCard ${props.pokemon.extra.types[props.pokemon.extra.types.length -1].type.name}`}>
+            <CardHeader pokemon={props.pokemon} />
 
+            {props.pokemon.extra.id === null ? (
+            <p>Loading...</p>
+            ) : (
+            <>
+                <img
+                className="sprite"
+                alt={`A pokemon named ${props.pokemon.name}`}
+                src={props.pokemon.extra.sprites.front_default}
+                />
+                <div className="add-remove-button" onClick={props.handleAddPokemon}>
+                CATCH
+                </div>
+            </>
+            )}
+        </div>
+        )
+    }
+
+    </div>
+
+      
+  );
+};
 
 export default PokemonCard;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { BrowserRouter } from "react-router-dom";
 // import { createLogger } from 'redux-logger'
 
-import reducer from './reducers';
+import reducer from "./reducers";
 
 // const logger = createLogger({})
-const store = createStore(reducer, applyMiddleware(thunk,
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    thunk
     // logger
-));
+  )
+);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>
-    , document.getElementById('root'));
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
